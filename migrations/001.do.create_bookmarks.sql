268adc5ccc29996ce482f19665a2d5698a48121b
ysz951 <shengyang_zhou@baylor.edu>
2020-08-02 03:00:49 -0500

    check bug

--- migrations/001.do.create_bookmarks.sql
+++ migrations/001.do.create_bookmarks.sql
@@ -7,3 +7,10 @@ CREATE TABLE bookmarks (
     description TEXT,
     rating star_rating NOT NULL
 );
+
+INSERT INTO bookmarks (title, url, description, rating)
+VALUES
+('Thinkful', 'https://www.thinkful.com', 'Think outside the classroom', '5'),
+('Google', 'https://www.google.com', 'Where we find everything else', '4'),
+('MDN', 'https://developer.mozilla.org', 'The only place to find web documentation', '5');
+
